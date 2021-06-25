3092448e3778ac58eaaa1965fdb4fcabe55f62a6
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2021-06-25 09:58:07 +0900

    feat: 1. 뷰 영역 소스 init

---
+++ src/main/java/com/exam/shop/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.exam.shop.controller;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "/home";
+    }
+}
