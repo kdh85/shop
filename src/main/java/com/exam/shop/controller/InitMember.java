30b9f2cdea6db77c9be56385b26160d5d4699864
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2021-06-25 11:44:49 +0900

    feat: 1. profile="local"일때, batch_size = 1000 설정 추가. 2. 테스트용 사용자 정보 생성 방식 변경  - 단건 단위-> 1000건 단위 벌크.

--- src/main/java/com/exam/shop/controller/InitMember.java
+++ src/main/java/com/exam/shop/controller/InitMember.java
@@ -12,6 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Profile(value = "local")
 @Component
@@ -23,7 +26,8 @@ public class InitMember {
     @PostConstruct
     public void init(){
         //initMemberService.createMemberInfo();
-        initMemberService.createMemberInfoByRepo();
+        //initMemberService.createMemberInfoByRepo();
+        initMemberService.createBulkMemberInfoByRepo();
     }
 
     @Component
@@ -37,17 +41,40 @@ public class InitMember {
 
         @Transactional
         public void createMemberInfo(){
+            Random rd = new Random();
+
             for (int i=1; i<101; i++){
-                Member member = Member.makeMemberWithAddress("user" + i, i, new Address("서울", String.valueOf(i * 10), String.valueOf(Math.random() * 100000)));
+                int zipcode = rd.nextInt(1000000-1);
+                String zipcodeFormat = String.format("%06d", zipcode);
+                Member member = Member.makeMemberWithAddress("user" + i, i, new Address("서울", String.valueOf(i * 10), zipcodeFormat));
                 entityManager.persist(member);
             }
         }
 
+        //건단위 insert.
         public void createMemberInfoByRepo(){
+            Random rd = new Random();
+
             for (int i=1; i<101; i++){
-                Member member = Member.makeMemberWithAddress("user" + i, i, new Address("서울", String.valueOf(i * 10), String.valueOf(Math.random() * 100000)));
+                int zipcode = rd.nextInt(1000000-1);
+                String zipcodeFormat = String.format("%06d", zipcode);
+                Member member = Member.makeMemberWithAddress("user" + i, i, new Address("서울", String.valueOf(i * 10), zipcodeFormat));
                 memberRepository.save(member);
             }
+        }
+
+        //batch_size 단위로 bulk insert.
+        public void createBulkMemberInfoByRepo(){
+            List<Member> members = new ArrayList<>();
+            Random rd = new Random();
+
+            for (int i=1; i<101; i++){
+                int zipcode = rd.nextInt(1000000-1);
+                String zipcodeFormat = String.format("%06d", zipcode);
+                Member member = Member.makeMemberWithAddress("user" + i, i, new Address("서울", String.valueOf(i * 10), zipcodeFormat));
+                members.add(member);
+            }
+            memberRepository.saveAll(members);
         }
     }
 }
