503ebf9f8bad8900fabf95aa6bf9d9f0cc50158d
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2021-06-25 10:12:51 +0900

    feat: 1. MemberService class 생성. 2. 사용자 추가 개발. 3. 검색조건을 통한 사용자 목록 조회(페이징 처리).
    - 서비스단위 개발 junit test 포함.

---
+++ src/main/java/com/exam/shop/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.exam.shop.service;
+
+import com.exam.shop.domain.dto.MemberDto;
+import com.exam.shop.domain.dto.MemberForm;
+import com.exam.shop.domain.entity.Address;
+import com.exam.shop.domain.entity.Member;
+import com.exam.shop.domain.search.MemberSearchCondition;
+import com.exam.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void join(MemberForm memberForm) {
+
+        memberRepository.save(
+                Member.makeMemberWithAddress(
+                        memberForm.getName(),
+                        memberForm.getAge(),
+                        new Address(
+                                memberForm.getCity(),
+                                memberForm.getStreet(),
+                                memberForm.getZipcode()
+                        ))
+        );
+    }
+
+    public Page<MemberDto> findMemberByCondition(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.findMemberByCondition(condition, pageable);
+    }
+}
---
+++ src/test/java/com/exam/shop/service/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package com.exam.shop.service;
+
+import com.exam.shop.domain.dto.MemberDto;
+import com.exam.shop.domain.dto.MemberForm;
+import com.exam.shop.domain.search.MemberSearchCondition;
+import com.exam.shop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Commit
+    void joinTest() {
+        MemberForm newMember = new MemberForm();
+        newMember.setName("user0");
+        newMember.setCity("서울");
+        newMember.setStreet("송파");
+        newMember.setZipcode("123321");
+        memberService.join(newMember);
+    }
+
+    @Test
+    void searchTest(){
+
+        MemberForm newMember = new MemberForm();
+        newMember.setName("user0");
+        newMember.setCity("서울");
+        newMember.setStreet("송파");
+        newMember.setZipcode("123321");
+        memberService.join(newMember);
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setUsername("user0");
+        Page<MemberDto> memberByCondition = memberRepository.findMemberByCondition(condition, pageRequest);
+
+        assertThat(memberByCondition.getContent()).extracting("name").containsExactly("user0");
+    }
+}
