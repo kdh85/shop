ae1addc57381cc612566baadaec38249cd6d18b4
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2021-07-16 14:30:18 +0900

    feat: 주문서비스 테스트 코드 수정.

--- src/test/java/com/exam/shop/service/OrdersServiceTest.java
+++ src/test/java/com/exam/shop/service/OrdersServiceTest.java
@@ -2,12 +2,11 @@ package com.exam.shop.service;
 
 import com.exam.shop.domain.dto.ItemForm;
 import com.exam.shop.domain.dto.MemberForm;
-import com.exam.shop.domain.search.OrderSearchCondition;
 import com.exam.shop.domain.dto.OrdersDto;
-import com.exam.shop.domain.entity.Item;
 import com.exam.shop.domain.entity.Member;
 import com.exam.shop.domain.entity.OrderStatus;
 import com.exam.shop.domain.entity.itemtype.Book;
+import com.exam.shop.domain.search.OrderSearchCondition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,7 +58,6 @@ class OrdersServiceTest {
 
     @Test
     void ordersCreateAndSearchTest() {
-        System.out.println("----------ordersCreateTest call---------");
         Member findMember = memberService.findByUsername("user0");
 
         Optional<Book> findItem = itemService.findByBook("book1");
@@ -81,28 +79,23 @@ class OrdersServiceTest {
 
     @Test
     void orderCancelTest() {
-        System.out.println("----------orderCancelTest call---------");
         //주문생성.
         Optional<Member> findMember = memberService.findById(1L);
 
-        Optional<Item> findItem = itemService.findById(1L);
+        Optional<Book> findBook = itemService.findByBook("book1");
 
-        if (findMember.isPresent() && findItem.isPresent()) {
-            Long orderId = ordersService.ordersCreate(findMember.get().getId(), findItem.get().getId(), 10);
+        if (findMember.isPresent() && findBook.isPresent()) {
+            Long orderId = ordersService.ordersCreate(findMember.get().getId(), findBook.get().getId(), 10);
             entityManager.flush();
             entityManager.clear();
             ordersService.ordersCancel(orderId);
         }
-
 
         OrderSearchCondition condition = new OrderSearchCondition();
         condition.setMemberName("user0");
         condition.setOrderStatus(OrderStatus.CANCEL);
 
         List<OrdersDto> ordersDtos = ordersService.searchOrdersWithItems(condition);
-//        for (OrdersDto ordersDto : ordersDtos) {
-//            System.out.println("ordersDto = " + ordersDto);
-//        }
-//        assertThat(ordersDtos.get(0).getOrderStatus()).isEqualTo(OrderStatus.CANCEL);
+        assertThat(ordersDtos).extracting("orderStatus").containsExactly(OrderStatus.CANCEL);
     }
 }
