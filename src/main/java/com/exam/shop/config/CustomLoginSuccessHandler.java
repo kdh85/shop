4a19f0be266e1dc2f1b0108f18179d2e98e7acc5
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2022-07-14 16:49:16 +0900

    refactor: * 로그인 성공시 home으로 가도록 수정.

--- src/main/java/com/exam/shop/config/CustomLoginSuccessHandler.java
+++ src/main/java/com/exam/shop/config/CustomLoginSuccessHandler.java
@@ -1,20 +1,17 @@
 package com.exam.shop.config;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
-        //super.onAuthenticationSuccess(request, response, authentication);
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        response.sendRedirect("/");
+        response.sendRedirect("/home");
     }
 }
